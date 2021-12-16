bfdd5cdd59df38b4ffd4ece1fe64bdd6a2a5a03b
koenzi132 <daniel.k9159@gmail.com>
2021-12-17 08:34:47 +0900

    TabBar Controller

---
+++ MakingPractice/05Tabbar/05Tabbar/FourthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FourthViewController.swift
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        appDelegate.fourthVC = self
+    }
+    
+}
---
+++ MakingPractice/05Tabbar/05Tabbar/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let index = self.tabBarController?.selectedIndex {
+            print("tabIndex", index)
+        } else {
+            print("nil")
+        }
+    }
+
+    @IBAction func onBtnNext(_ sender: UIButton) {
+        self.tabBarController?.selectedIndex = 1 // 인덱스값
+    }
+}
+
